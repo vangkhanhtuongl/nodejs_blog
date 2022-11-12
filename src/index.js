b75268342731bd58230fce4f58f363acdf5db26c
Khanh Tuong Vang <vangkhanhtuong7743@gmail.com>
2022-11-12 18:47:50 +0700

    Su dung Template Engine

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const { engine }=require('express-handlebars')
+const { extname } = require('path')
 const app = express()
-
+//HTTP Logger
 app.use(morgan('combined'))
-
-app.engine('handlebars',engine())
-app.set('view engine','handlebars')
-app.set('views',path.join(__dirname,'views'))
+//Template Engine
+app.engine('hbs',engine({
+    extname: '.hbs'
+}))
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'resource/views'))
 
 app.get('/',(req, res) => 
   res.render('Home'))
+
+app.get('/news',(req, res) => 
+  res.render('News'))
 
 app.listen(8000,()=>console.log(`Example app listening at https://localhost:8000`))
